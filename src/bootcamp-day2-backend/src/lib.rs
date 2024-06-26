5b154cc4bdc89dc13d44d652b6da8d72ca087cde
draszec <draszec@gmail.com>
2024-06-26 23:45:53 +0200

    add styles

--- src/bootcamp-day2-backend/src/lib.rs
+++ src/bootcamp-day2-backend/src/lib.rs
@@ -22,3 +22,20 @@ fn read_posts() -> Vec<String> {
         posts.borrow().clone()
     })
 }
+
+#[ic_cdk::update]
+fn delete_post(post_id: usize){
+    POSTS.with(|posts: &RefCell<Vec<String>>| {
+        posts.borrow_mut().remove(post_id)
+    });
+}
+
+#[ic_cdk::update]
+fn edit_post(post_id: usize, new_post: String) {
+    POSTS.with(|posts: &RefCell<Vec<String>>| {
+        let mut binding: std::cell::RefMut<Vec<String>> = posts.borrow_mut();
+        let post: Option<&mut String> = binding.get_mut(post_id);
+        let old_post: &mut String = post.unwrap();
+        *old_post = new_post;
+    });
+}
--- src/bootcamp-day2-frontend/src/components/Blog.vue
+++ src/bootcamp-day2-frontend/src/components/Blog.vue
@@ -1,11 +1,20 @@
 <template>
     <div>
-        <button @click="loadPosts">refresh</button>
-        This is an example blog
-        {{ posts }}
-        <input v-model="newBlog" type="text">
-        <button @click="addPosts">addPosts</button>
-    </div>
+        <h2 class="text-blue-600">Blog posts:</h2>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="loadPosts" class="bg-blue-600 rounded-sm text-white p-4">refresh</button>
+        </div>
+        <div class="grid mx-6 gap-4 my-4">
+            <div v-for="post in posts"
+            class="drop-shadow-xl bg-stone-300 p-4">
+                <p>{{ post }}</p>
+            </div>
+        </div>
+        <div class="flex justify-center flex-col">
+            <input v-model="newBlog" class="border-2 border-blue-600 p-4" type="text">
+            <button @click="addPosts" class="bg-blue-600 rounded-sm text-white p-4">addPosts</button>
+        </div>
+        </div>
 </template>
 
 <script>
@@ -26,5 +35,8 @@ export default {
             this.posts = await bootcamp_day2_backend.read_posts();
         }
     },
+    async mounted(){
+        this.loadPosts()
+    }
 }
 </script>
